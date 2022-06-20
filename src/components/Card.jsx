0793ba75687177ec251113681a5178b92e8cf202
Chaos <antonioscutari2@hotmail.com>
2022-06-20 20:42:23 -0300

    [FIX] Corrected spaces and rendering

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,25 +3,15 @@ import React from "react";
 export default function Card(props) {
   const { max, min, name, img, onClose } = props;
   return (
-    <div>
+    <div className="cardCity">
       <button onClick={onClose}>x</button>
       <h4>{name}</h4>
-      <div>
-        {" "}
-        Max.
-        <div>{max}°</div>
-      </div>
-      <div>
-        {" "}
-        Min.
-        <div>{min}°</div>
-      </div>
-      <div>
-        <img
-          src={`http://openweathermap.org/img/wn/${img}@2x.png`}
-          alt="Ícono del clima"
-        />
-      </div>
+      <div>Max. {max}°</div>
+      <div>Min. {min}°</div>
+      <img
+        src={`http://openweathermap.org/img/wn/${img}@2x.png`}
+        alt="weatherIcon"
+      />
     </div>
   );
 }
