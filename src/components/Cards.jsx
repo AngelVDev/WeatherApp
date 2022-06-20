e1544e21390a7dbb0584d4a9a424ea7398513729
Chaos <antonioscutari2@hotmail.com>
2022-06-20 20:43:59 -0300

    [FIX] Replaced Link for <a/> and rendering is working as intended. onClose still needs feex

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import Card from "./Card";
-
 export default function Cards({ cities }) {
   // acá va tu código
   // tip, podés usar un map
   console.log(cities);
   return (
-    <div>
+    <div className="cityContainer">
       {cities.map((city) => {
         return (
-          <div>
+          <a href={`/city/${city.id}`}>
             <Card
-              max={city.main.temp_max}
-              min={city.main.temp_min}
+              max={city.max}
+              min={city.min}
               name={city.name}
-              img={city.weather[0].icon}
-              onClose={() => alert(city.name)}
+              img={city.img}
+              onClose={() => city.id}
             />
-          </div>
+          </a>
         );
       })}
     </div>
