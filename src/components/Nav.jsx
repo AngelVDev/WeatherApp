07f6a0e7859f7e1084be36d9830e5961095b12c2
Chaos <antonioscutari2@hotmail.com>
2022-06-19 23:51:58 -0300

    [FIX] This is weird, but it's working

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,27 +1,24 @@
-import React from 'react';
-import Logo from '../img/logoHenry.png'
-import SearchBar from './SearchBar.jsx';
-import './Nav.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Logo from "../img/logoHenry.png";
+import SearchBar from "./SearchBar.jsx";
+import "./Nav.css";
+import { Link } from "react-router-dom";
 
-
-function Nav({onSearch}) {
+function Nav({ onSearch }) {
   return (
     <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand">
+      <span className="navbar-brand">
         <Link to="/">
           <img id="logoHenry" src={Logo} alt="" />
-          Report
-        </ Link>
-        </span>
-        <SearchBar
-          onSearch={onSearch}
-        />
-        <Link to="/about">
-          <span>About</span>
+          WEATHER REPORT
         </Link>
+      </span>
+      <SearchBar onSearch={onSearch} />
+      <Link to="/about">
+        <span>About</span>
+      </Link>
     </nav>
   );
-};
+}
 
 export default Nav;
