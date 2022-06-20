197c0abf0776da1de6e35ed890aa0228494ca873
Chaos <antonioscutari2@hotmail.com>
2022-06-19 23:52:43 -0300

    [FIX] Now working, but APIKey isn't

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
 
-export default function SearchBar({onSearch}) {
+export default function SearchBar({ onSearch }) {
   const [city, setCity] = useState("");
   return (
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      onSearch(city);
-    }}>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSearch(city);
+      }}
+    >
       <input
         type="text"
-        placeholder="Ciudad..."
+        placeholder="City..."
         value={city}
-        onChange={e => setCity(e.target.value)}
+        onChange={(e) => setCity(e.target.value)}
       />
-      <input type="submit" value="Agregar" />
+      <input type="submit" />
     </form>
   );
 }
