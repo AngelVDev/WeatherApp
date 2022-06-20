0a35bf4fa0ebc395f41307f1c0997954747ace62
Chaos <antonioscutari2@hotmail.com>
2022-06-20 20:41:25 -0300

    [FIXED] Now rendering

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,9 @@
 import React from "react";
+import "./styles/About.css";
 export default function About() {
   return (
     <div className="about">
       <p>
-        {" "}
         Done by Angel Scutari for HENRY Bootcamp. This was a homework exercise
       </p>
     </div>
