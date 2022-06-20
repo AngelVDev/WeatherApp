f8e0999d460a72c296cfc03a9cad2f998c9d387f
Chaos <antonioscutari2@hotmail.com>
2022-06-19 23:54:42 -0300

    [???] I'm trying to understand this mess

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import styles from './About.css'
-export default function About () {
-    return (
-        <div className={styles.about}>
-            <p>Done by Angel Scutari for HENRY Bootcamp. This was a homework</p>
-        </div>
-    )
+import styles from "./About.css";
+export default function About() {
+  return (
+    <div className={styles.about}>
+      <p>
+        {" "}
+        Done by Angel Scutari for HENRY Bootcamp. This was a homework exercise
+      </p>
+    </div>
+  );
 }
