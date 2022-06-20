a532dd027d3354bbbf95a44795faad4182eef7e8
Chaos <antonioscutari2@hotmail.com>
2022-06-19 23:51:23 -0300

    [FIX] Trying to make this work, and make it cleaner. What a mess

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
-import './App.css';
-import Nav from './components/Nav.jsx';
-import Cards from './components/Cards.jsx';
-import About from './components/About.jsx';
-import City from './components/City.jsx'
-//import CityClass from './components/CityClass';
-const apiKey = 'fecb4768ab7de692bbbe291b87984348';
+import React, { useState } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+import Nav from "./components/Nav.jsx";
+import Cards from "./components/Cards.jsx";
+import About from "./components/About.jsx";
+import City from "./components/City.jsx";
+const apiKey = "fecb4768ab7de692bbbe291b87984348";
 
 function App() {
   const [cities, setCities] = useState([]);
   function onClose(id) {
-    setCities(oldCities => oldCities.filter(c => c.id !== id));
+    setCities((oldCities) => oldCities.filter((c) => c.id !== id));
   }
-  function onSearch(ciudad) {
+  function onSearch(city) {
     //Llamado a la API del clima
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&units=metric&appid=${apiKey}`)
-      .then(r => r.json())
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+    )
+      .then((r) => r.json())
       .then((recurso) => {
-        if(recurso.main !== undefined){
+        if (recurso.main !== undefined) {
           const ciudad = {
             min: Math.round(recurso.main.temp_min),
             max: Math.round(recurso.main.temp_max),
@@ -30,37 +31,33 @@ function App() {
             weather: recurso.weather[0].main,
             clouds: recurso.clouds.all,
             latitud: recurso.coord.lat,
-            longitud: recurso.coord.lon
+            longitud: recurso.coord.lon,
           };
-          setCities(oldCities => [...oldCities, ciudad]);
+          setCities((oldCities) => [...oldCities, ciudad]);
         } else {
           alert("Ciudad no encontrada");
         }
       });
   }
   function onFilter(ciudadId) {
-    let ciudad = cities.filter(c => c.id === parseInt(ciudadId));
-    if(ciudad.length > 0) {
-        return ciudad[0];
+    let ciudad = cities.filter((c) => c.id === parseInt(ciudadId));
+    if (ciudad.length > 0) {
+      return ciudad[0];
     } else {
-        return null;
+      return null;
     }
   }
   return (
     <div className="App">
-      <Nav onSearch={onSearch}/>
-        <BrowserRouter>
-          <Route path="/" element={
-            <Cards
-              cities={cities}
-              onClose={onClose}
-            />}
-            />
-            <Route path="/about" element={<About />}/>
-            <Route path="/ciudad/:ciudadId" element={< City onFilter={onFilter}/>} />
-            {/* <Route path="/probandoSinHooks/:hola" element={< CityClass />} /> */}
-        </BrowserRouter>
-
+      <BrowserRouter>
+        <Nav onSearch={onSearch} />
+        <Route path="/" element={<Cards cities={cities} onClose={onClose} />} />
+        <Route path="/about" element={About} />
+        <Route
+          path="/ciudad/:ciudadId"
+          element={<City onFilter={onFilter} />}
+        />
+      </BrowserRouter>
     </div>
   );
 }
